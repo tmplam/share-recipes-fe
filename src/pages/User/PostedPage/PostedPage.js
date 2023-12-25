7056e905aed5ae7a4ec5aed9c2302a7b71803abf
21120492 <tranmyphulam123@gmail.com>
2023-12-25 21:47:50 +0700

    Add hide/unhide recipe functionality

--- src/pages/User/PostedPage/PostedPage.js
+++ src/pages/User/PostedPage/PostedPage.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import { useEffect, useRef, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
@@ -23,7 +23,7 @@ function FavouritePage() {
 
     const [keyword, setKeyword] = useState('');
     const keywordRef = useRef();
-    const [favouriteList, setFavouriteList] = useState([]);
+    const [postedList, setPostedList] = useState([]);
     const [categoryList, setCategoryList] = useState([]);
 
     useEffect(() => {
@@ -40,10 +40,10 @@ function FavouritePage() {
                 const data = response.data;
                 setPage(data.page);
                 setTotalPage(data.total_page);
-                setFavouriteList(data.data);
+                setPostedList(data.data);
             })
             .catch((err) => {
-                // console.log(err);
+                // console.error(err);
             });
     }, [page, keyword, category, auth]);
 
@@ -55,7 +55,7 @@ function FavouritePage() {
                 setCategoryList(data.data);
             })
             .catch((err) => {
-                toast.error(err?.response?.data?.message | 'Lỗi server!');
+                toast.error(err?.response?.data?.message || 'Lỗi server!');
             });
     }, []);
 
@@ -68,17 +68,38 @@ function FavouritePage() {
         setKeyword(keywordRef.current.value);
     }
 
-    function handleDeleteFavourite(recipeId) {
+    const [deleteId, setDeleteId] = useState('');
+    function handleDeleteBtnClick(recipeId) {
+        setDeleteId(recipeId);
+        handleShow();
+    }
+
+    // DELETE modal
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    function handleDeleteRecipe() {
+        handleClose();
         axios
-            .delete(`user/favourites/${recipeId}`, {
-                headers: {
-                    Authorization: auth.token,
+            .put(
+                `/recipes/${deleteId}/status`,
+                {
+                    status: 'Deleted',
                 },
-            })
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: auth?.token === 'EXPIRED' ? null : auth?.token,
+                    },
+                },
+            )
             .then((response) => {
-                const data = response.data;
-                toast.success(data.message);
-                setFavouriteList((prev) => prev.filter((recipe) => recipe.recipeid !== recipeId));
+                // const data = response.data;
+                // toast.success(data.message);
+                toast.success('Xóa thức thành công!');
+                setPostedList((prev) => prev.filter((recipe) => recipe.recipeid !== deleteId));
             })
             .catch((err) => {
                 toast.error(err.response.data.message);
@@ -86,63 +107,90 @@ function FavouritePage() {
     }
 
     return (
-        <Container className={cx('wrapper')}>
-            <h1 className={cx('title')}>
-                Công Thức Đã Đăng <FontAwesomeIcon icon={faBookOpen} />
-            </h1>
-            <div className={cx('main')}>
-                <div className={cx('control')}>
-                    <form id="search-form" className={cx('search')} onSubmit={handleSubmitSearch}>
-                        <input
-                            ref={keywordRef}
-                            placeholder="Tìm kiếm ..."
-                            spellCheck={false}
-                            name="search"
-                        />
-                        <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        </button>
-                    </form>
-
-                    <div className={cx('filter-control')}>
-                        <label className={cx('filter-label')} htmlFor="category">
-                            Loại:
-                        </label>
-
-                        <select
-                            onChange={(e) => setCategory(e.target.value)}
-                            className={cx('filter-select')}
-                            id="category"
-                        >
-                            <option value="all">Tất cả</option>
-                            {categoryList.map((category) => (
-                                <option key={category.categoryid} value={category.categoryid}>
-                                    {category.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
+        <>
+            <Container className={cx('wrapper')}>
+                <div className={cx('title-wrapper')}>
+                    <h1 className={cx('title')}>
+                        Công Thức Đã Đăng <FontAwesomeIcon icon={faBookOpen} />
+                    </h1>
                 </div>
-                <Row className="gx-3">
-                    {favouriteList.map((recipe) => (
-                        <Col xs={12} md={6} lg={4} xl={3} key={recipe.recipeid}>
-                            <PostedItem onDelete={handleDeleteFavourite} {...recipe} />
-                        </Col>
-                    ))}
-                </Row>
-                {totalPage >= 2 ? (
-                    <div className={cx('pagination-wrapper')}>
-                        <Pagination
-                            page={page}
-                            total_page={totalPage}
-                            onPageChange={handlePageChange}
-                        />
+                <div className={cx('main')}>
+                    <div className={cx('control')}>
+                        <form
+                            id="search-form"
+                            className={cx('search')}
+                            onSubmit={handleSubmitSearch}
+                        >
+                            <input
+                                ref={keywordRef}
+                                placeholder="Tìm kiếm ..."
+                                spellCheck={false}
+                                name="search"
+                            />
+                            <button className={cx('search-btn')}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </button>
+                        </form>
+
+                        <div className={cx('filter-control')}>
+                            <label className={cx('filter-label')} htmlFor="category">
+                                Loại:
+                            </label>
+
+                            <select
+                                onChange={(e) => setCategory(e.target.value)}
+                                className={cx('filter-select')}
+                                id="category"
+                            >
+                                <option value="all">Tất cả</option>
+                                {categoryList.map((category) => (
+                                    <option key={category.categoryid} value={category.categoryid}>
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-                ) : (
-                    false
-                )}
-            </div>
-        </Container>
+                    <Row className="gx-3">
+                        {postedList.map((recipe) => (
+                            <Col xs={12} md={6} lg={4} xl={3} key={recipe.recipeid}>
+                                <PostedItem onDelete={handleDeleteBtnClick} {...recipe} />
+                            </Col>
+                        ))}
+                    </Row>
+                    {totalPage >= 2 ? (
+                        <div className={cx('pagination-wrapper')}>
+                            <Pagination
+                                page={page}
+                                total_page={totalPage}
+                                onPageChange={handlePageChange}
+                            />
+                        </div>
+                    ) : (
+                        false
+                    )}
+                </div>
+            </Container>
+
+            <Modal centered show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        <h2 style={{ margin: 0 }}>Xác nhận!</h2>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p style={{ margin: 0 }}>Một phát nữa là đi luôn đó shop!!!</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button className={cx('modal-btn')} onClick={handleClose}>
+                        Hủy bỏ
+                    </button>
+                    <button className={cx('modal-btn', 'delete')} onClick={handleDeleteRecipe}>
+                        Xóa
+                    </button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
--- src/pages/User/PostedPage/components/PostedItem/PostedItem.js
+++ src/pages/User/PostedPage/components/PostedItem/PostedItem.js
@@ -4,9 +4,13 @@ import { faTrashCan, faEye, faEyeSlash, faPenToSquare } from '@fortawesome/free-
 
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
+import axios from '~/utils/api';
+import useAuth from '~/hooks/useAuth';
+import { toast } from 'react-toastify';
 
 import styles from './PostedItem.module.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
@@ -20,9 +24,13 @@ function PostedItem({
     description,
     reviews,
     category,
+    status,
     onDelete,
-    onHide,
 }) {
+    const { auth } = useAuth();
+
+    const [recipeStatus, setRecipeStatus] = useState(status);
+
     function handleDelete(e) {
         e.preventDefault();
         onDelete(recipeid);
@@ -30,43 +38,103 @@ function PostedItem({
 
     function handleHide(e) {
         e.preventDefault();
-        onHide(recipeid);
+        axios
+            .put(
+                `/recipes/${recipeid}/status`,
+                {
+                    status: 'Hidden',
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: auth?.token === 'EXPIRED' ? null : auth?.token,
+                    },
+                },
+            )
+            .then((response) => {
+                // const data = response.data;
+                // toast.success(data.message);
+                toast.success('Ẩn công thức thành công!');
+                setRecipeStatus('Hidden');
+            })
+            .catch((err) => {
+                toast.error(err.response.data.message);
+            });
+    }
+
+    function handleUnhide(e) {
+        e.preventDefault();
+        axios
+            .put(
+                `/recipes/${recipeid}/status`,
+                {
+                    status: 'Approved',
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: auth?.token === 'EXPIRED' ? null : auth?.token,
+                    },
+                },
+            )
+            .then((response) => {
+                // const data = response.data;
+                // toast.success(data.message);
+                toast.success('Bỏ ẩn công thức thành công!');
+                setRecipeStatus('Approved');
+            })
+            .catch((err) => {
+                toast.error(err.response.data.message);
+            });
     }
 
     return (
-        <Link
-            className={cx('wrapper')}
-            to={`/recipes/${recipeid}`}
-            style={{ backgroundImage: `url("${recipeavatar}")` }}
-        >
-            <div className={cx('inner-wrapper')}>
-                <div className={cx('head')}>
-                    <div className={cx('control')}>
-                        <Tippy delay={[0, 50]} content="Sửa" placement="auto">
-                            <Link to={`/recipes/${recipeid}/edit`} className={cx('edit-btn')}>
-                                <FontAwesomeIcon icon={faPenToSquare} />
-                            </Link>
-                        </Tippy>
-                        <Tippy delay={[0, 50]} content="Xóa" placement="auto">
-                            <button onClick={handleDelete} className={cx('delete-btn')}>
-                                <FontAwesomeIcon icon={faTrashCan} />
-                            </button>
-                        </Tippy>
+        <div className={cx('outer-wrapper')}>
+            <Link
+                className={cx('wrapper')}
+                to={`/recipes/${recipeid}`}
+                style={{ backgroundImage: `url("${recipeavatar}")` }}
+            >
+                <div className={cx('inner-wrapper')}>
+                    <div className={cx('recipe-info')}>
+                        <h2 className={cx('name')}>{name}</h2>
+                        <p>
+                            <strong>Loại: </strong>Đồ ăn nhanh
+                        </p>
                     </div>
+                </div>
+            </Link>
+
+            <div className={cx('head')}>
+                <div className={cx('control')}>
+                    <Tippy delay={[0, 50]} content="Sửa" placement="auto">
+                        <Link to={`/recipes/${recipeid}/edit`} className={cx('edit-btn')}>
+                            <FontAwesomeIcon icon={faPenToSquare} />
+                        </Link>
+                    </Tippy>
+                    <Tippy delay={[0, 50]} content="Xóa" placement="auto">
+                        <button onClick={handleDelete} className={cx('delete-btn')}>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </Tippy>
+                </div>
+                {recipeStatus === 'Approved' ? (
                     <Tippy delay={[0, 50]} content="Ẩn" placement="auto">
                         <button onClick={handleHide} className={cx('hidden-btn')}>
                             <FontAwesomeIcon icon={faEye} className={cx('hidden-icon')} />
                         </button>
                     </Tippy>
-                </div>
-                <div className={cx('recipe-info')}>
-                    <h2 className={cx('name')}>{name}</h2>
-                    <p>
-                        <strong>Loại: </strong>Đồ ăn nhanh
-                    </p>
-                </div>
+                ) : recipeStatus === 'Hidden' ? (
+                    <Tippy delay={[0, 50]} content="Bỏ ẩn" placement="auto">
+                        <button onClick={handleUnhide} className={cx('hidden-btn')}>
+                            <FontAwesomeIcon icon={faEyeSlash} className={cx('hidden-icon')} />
+                        </button>
+                    </Tippy>
+                ) : (
+                    'Pending/Deleted'
+                )}
             </div>
-        </Link>
+        </div>
     );
 }
 
